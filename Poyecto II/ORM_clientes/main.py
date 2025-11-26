2f911b9b738bf01928b8edee88db7e5a85156bfc
Gustavo Rosa <grosar03@gmail.com>
2025-11-25 21:34:29 -0300

    Delete Poyecto II/ORM_clientes/main.py

--- Poyecto II/ORM_clientes/main.py
+++
@@ -1,6 +0,0 @@
-from database import engine, Base
-import models 
-
-print("Inicializando la base de datos y creando tablas...")
-Base.metadata.create_all(bind=engine)
-print("Base de datos 'restaurante.db' creada con éxito.")
